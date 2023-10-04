fb2857ec9ba8da2171059af8b5e967b41eaca1e6
silvaRaphael <raphaeltiago02@gmail.com>
2023-10-04 11:13:42 -0300

    done

--- src/infra/http/web/routes/home-routes.ts
+++ src/infra/http/web/routes/home-routes.ts
@@ -34,22 +34,19 @@ router.get(
 			created_at: new Date(new Date().setDate(new Date().getDate() - 1)),
 		});
 
-		const ordersPercentage =
-			(todayOrders.length * 100) / yesterdayOrders.length - 100;
-
 		const monthOrders = await getAllOrdersUseCase.execute({
 			created_in: [
-				new Date(new Date().setMonth(new Date().getMonth() - 1)),
-				new Date(new Date().setMonth(new Date().getMonth())),
+				new Date(new Date().getFullYear(), new Date().getMonth()),
+				new Date(new Date().getFullYear(), new Date().getMonth() + 1),
 			],
 		});
 
-		// const lastMonthOrders = await getAllOrdersUseCase.execute({
-		// 	created_in: [
-		// 		new Date(new Date().setMonth(new Date().getMonth() - 1)),
-		// 		new Date(new Date().setMonth(new Date().getMonth())),
-		// 	],
-		// });
+		const lastMonthOrders = await getAllOrdersUseCase.execute({
+			created_in: [
+				new Date(new Date().getFullYear(), new Date().getMonth() - 1),
+				new Date(new Date().getFullYear(), new Date().getMonth()),
+			],
+		});
 
 		const clients = await getAllClientsUseCase.execute();
 
@@ -57,7 +54,7 @@ router.get(
 			(acc, crr) => acc + crr.price,
 			0,
 		);
-		const lastMonthRevenueValue = monthOrders.reduce(
+		const lastMonthRevenueValue = lastMonthOrders.reduce(
 			(acc, crr) => acc + crr.price,
 			0,
 		);
@@ -67,20 +64,23 @@ router.get(
 			currency: 'BRL',
 		}).format(monthRevenueValue);
 
-		const monthRevenuePercentage =
-			(monthRevenueValue * 100) / lastMonthRevenueValue - 100;
-
 		const clientsAmmount = clients.length;
 
 		res.render('pages/home', {
 			title: 'Pizzaria',
 			...pageContext(req),
 			ordersAmmount: todayOrders.length,
-			ordersPercentage: Math.abs(ordersPercentage || 0),
-			ordersIncrease: ordersPercentage >= 0,
+			ordersPercentage: (
+				(Math.abs(todayOrders.length - yesterdayOrders.length) * 100) /
+				(yesterdayOrders.length || 1)
+			).toFixed(0),
+			ordersIncrease: todayOrders.length - yesterdayOrders.length >= 0,
 			monthRevenue,
-			monthRevenuePercentage,
-			monthRevenueIncrease: monthRevenuePercentage >= 0,
+			monthRevenuePercentage: (
+				(Math.abs(monthRevenueValue - lastMonthRevenueValue) * 100) /
+				(lastMonthRevenueValue || 1)
+			).toFixed(0),
+			monthRevenueIncrease: monthRevenueValue - lastMonthRevenueValue >= 0,
 			clientsAmmount,
 		});
 	},
